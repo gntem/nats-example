b0cde06f5e900fadcdcaa8a91f2c93710957d3c1
Jose St Bernard <j.bernard@liquidlabs.de>
2018-01-04 17:22:09 +0100

    remove call to missing function

--- consumer/handlers.js
+++ consumer/handlers.js
@@ -1,27 +1,30 @@
-const { isDuplicated, isNextEvent } = require('./utils')
+const {
+  isDuplicated,
+  isNextEvent
+} = require('./utils')
 
 function onMessageReceived(msg) {
   const sequence = msg.getSequence()
   const data = JSON.parse(msg.getData())
-  if (shouldBeProcessed(msg)) {
 
-    log.info('Processing message with sequence %s', sequence)
-    log.trace('Message[%s]: %o', sequence, data)
+  log.info('Processing message with sequence %s', sequence)
+  log.trace('Message[%s]: %o', sequence, data)
 
-    if(isDuplicated(msg)) {
-      log.warning('Duplicated message with id: %id, not processing', data.eventId)
-      return
-    }
-    if(!isNextEvent(msg)) {
-      // REVISIT: request event that should come next on demand using setStartAtSequence to reduce waiting time
-      log.warning('Unordered event, stalling message')
-      return
-    }
-
-    // Add business logic here
-    msg.ack()
+  if (isDuplicated(msg)) {
+    log.warning('Duplicated message with id: %id, not processing', data.eventId)
     return
   }
+  if (!isNextEvent(msg)) {
+    // REVISIT: request event that should come next on demand using setStartAtSequence to reduce waiting time
+    log.warning('Unordered event, stalling message')
+    return
+  }
+
+  // Add business logic here
+  msg.ack()
+  return
 };
 
-module.exports = { onMessageReceived };
+module.exports = {
+  onMessageReceived
+};
